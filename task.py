31d2ed5a86a936a8f57368390889ce192ba767c5
Giambuzzi Mateo <mategiambu@gmail.com>
2026-01-07 18:42:42 -0300

    new methods

--- task.py
+++ task.py
@@ -1,51 +1,109 @@
 import sys
 
+comand_with_txt = {"add"}
+comand_with_id_and_txt = {"done"}
+
 class Task:
     def __init__(self):
         self.tasks = {}
+        self.ids = {}
         self.last_task = ()
         self.comands = {"add": Task.add,
                         "ls" : Task.ls,
-                        "salir": Task.salir}
+                        "quit": Task.quit,
+                        "done": Task.done,
+                        "remove": Task.remove}
         return
 
-    def add(self):
-        name = input("Por favor, ingresá la tarea:  ")
-        new_id = len(self.tasks) +1
+    def add(self,name):
 
-        self.tasks[new_id] = [name,"incomplete"]
-        self.last_task = (name, id)
+        new_id = len(self.tasks) +1
+        new_id = str(new_id)
+
+        self.tasks[new_id] = [name,"Incomplete"]
+        self.ids[name] = new_id
+        self.last_task = (name, new_id)
         
-        print(f"nueva tarea agregada! : {name}, su id es {new_id}")
         Task.fetch(self)
 
-    def fetch(self):
-        cmd = input("Que necesitas? Para cerrar el programa ingresa el comando 'salir' :  ")
-        Task.execute_comand(self,cmd)
+    def ls(self,name = None):
+        if name == "milton":
+            return print("NAAA MILTON PELOTUDO!!!!!!")
 
-    def ls(self):
-        return
+        if len(self.tasks) == 0:
+            print("No added tasks")
+        
+        for task in self.tasks:
+            task_name = self.tasks[task][0]
+            task_state = self.tasks[task][1]
+            print(f"task: {task_name}, id: {task}. {task_state}")
+        
+        Task.fetch(self)
 
-    def execute_comand(self,cmd):
+    def done(self,reference):
+        reference = str(reference)
+        
+        if reference in self.ids:
+            reference = self.ids[reference]
+
+        self.tasks[reference][1] = "done"
+
+        Task.fetch(self)
+
+    def remove(self,reference):
+        reference = str(reference)
+        
+        if reference in self.ids:
+            reference = self.ids[reference]
+
+        self.tasks.pop(reference)        
+
+    def execute_comand(self,cmd,name = None):
+        
         if cmd in self.comands.keys():
-            self.comands[cmd](self)
-        else: 
-            print("comando no valido")
-
-    def salir(self):
-        return print("Hasta luego!")
-    
-def main():
-    if len(sys.argv) > 2:
-        print("Uso: python tasker.py <cmd>")
+            self.comands[cmd](self,name)
+        
+        else:
+            return print("Command not valid")
+        
+    def quit(self,name = None):
         return
-    elif len(sys.argv) == 1:
-        cmd = input("Que necesitas? Para cerrar el programa ingresa el comando 'salir' :  ")
-    else:
-        cmd = sys.argv[1]
     
-    task = Task()
-    task.execute_comand(cmd)
+    def fetch(self):
+        cmd = input("'quit' to close the program:  ")
+
+        if cmd in comand_with_txt:
+            name = input("task:  ")
+            Task.execute_comand(self,cmd,name)
+        
+        else:
+            Task.execute_comand(self,cmd)
+
+def main():
+    
+    if len(sys.argv) > 3 or len(sys.argv) == 2:
+        print("Use: python tasker.py <cmd> <name_task>")
+        return
+    
+    elif len(sys.argv) == 3:
+        cmd = sys.argv[1]
+        name = sys.argv[2]
+
+        task = Task()  
+        task.execute_comand(cmd,name)
+    
+    else:
+        cmd = input("'quit' to close the program:  ")
+
+    if cmd in comand_with_txt:
+        name = input("task:  ")
+        task = Task()  
+        task.execute_comand(cmd,name)
+    elif cmd in comand_with_id_and_txt:
+        name = input("task or id:  ")
+    else:
+        task = Task()
+        task.execute_comand(cmd)
 
 if __name__ == "__main__":
     main()
